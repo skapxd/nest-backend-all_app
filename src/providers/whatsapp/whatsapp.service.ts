db4a5478af60b907842f8a1e262fa98d8b95d7fe
manuel <hbiaser132@gmail.com>
2021-08-28 15:09:29 -0500

    update whatsapp provider

--- src/providers/whatsapp/whatsapp.service.ts
+++ src/providers/whatsapp/whatsapp.service.ts
@@ -1,111 +1,34 @@
 import { WAConnection, MessageType, MessageOptions, Mimetype } from '@adiwajshing/baileys'
 import { Injectable } from '@nestjs/common';
 import * as fs from 'fs'
+import { async } from 'rxjs';
 
 @Injectable()
 export class WhatsAppService {
 
     private conn = new WAConnection();
 
-    constructor(){
-        if (!this.conn.phoneConnected) {
-            console.log('==========================');
-            console.log('logged');
-            console.log('==========================');
-            this.initConnection();
-        }
-    }
+    constructor() {
 
 
+        this.loadConnection()
+            .catch((e) => {
 
-    initConnection() {
-
-        this.loadAuthInfo()
-            .catch(async (err) => {
-
-                console.log('==========================');
-                console.log('Error');
-                console.log('==========================');
-
-                await this.connectFirstToWhatsApp()
+                this.initConnection();
             })
     }
 
 
-    private async connectFirstToWhatsApp() {
-
-        // const this.this.conn = new WAConnection()
-        // called when WA sends chats
-        // this can take up to a few minutes if you have thousands of chats!
-        this.conn.on('chats-received', async ({ hasNewChats }) => {
-            console.log(`you have ${this.conn.chats.length} chats, new chats available: ${hasNewChats}`)
-
-            const unread = await this.conn.loadAllUnreadMessages()
-            console.log("you have " + unread.length + " unread messages")
-        })
-        // called when WA sends chats
-        // this can take up to a few minutes if you have thousands of contacts!
-        this.conn.on('contacts-received', () => {
-            console.log('you have ' + Object.keys(this.conn.contacts).length + ' contacts')
-        })
-
-        this.conn.on('chat-update', chatUpdate => {
-            // `chatUpdate` is a partial object, containing the updated properties of the chat
-            // received a new message
-            if (chatUpdate.messages && chatUpdate.count) {
-                const message = chatUpdate.messages.all()[0]
-                console.log(message)
-            } else console.log(chatUpdate) // see updates (can be archived, pinned etc.)
-        })
-
-        this.conn.on('open', () => {
-            // save credentials whenever updated
-            console.log(`credentials updated!`)
-            const authInfo = this.conn.base64EncodedAuthInfo() // get all the auth info we need to restore this session
-            fs.writeFileSync('./auth_info.json', JSON.stringify(authInfo, null, '\t')) // save this info to a file
-        })
-
-        const qrAccess = fs.existsSync('./auth_info.json')
-
-        if (qrAccess) {
-            this.conn.loadAuthInfo('./auth_info.json') // will load JSON credentials from file
-
-        }
-
-        await this.conn.connect() // connect
-
-        console.log('==================================');
-        console.log('connections success first time');
-        console.log('==================================');
-
-    }
-
-    private async loadAuthInfo() {
-
-        const qrAccess = fs.existsSync('./auth_info.json')
-        if (!qrAccess) {
-
-            throw new Error('Error to connect')
-        }
-
-        this.conn.loadAuthInfo('./auth_info.json') // will load JSON credentials from file
-        await this.conn.connect()
-        console.log('==================================');
-        console.log('connections success for load access');
-        console.log('==================================');
-
-    }
-
 
     async sendSimpleText(data: any) {
 
-        
+
 
         if (!this.conn.phoneConnected) {
             console.log('==========================');
             console.log('logged');
             console.log('==========================');
-            this.initConnection();
+            this.loadConnection();
         }
 
 
@@ -128,4 +51,46 @@ export class WhatsAppService {
 
         // const sentMsg = await this.conn.sendMessage(id, { displayname: "Jeff", vcard: vcard }, MessageType.contact)
     }
+
+
+
+
+    initConnection(): Promise<void> {
+
+        this.conn = new WAConnection()
+
+        return new Promise(
+            async (resolve, reject) => {
+
+                try {
+
+                    await this.conn.connect()
+
+                    const authInfo = this.conn.base64EncodedAuthInfo() // get all the auth info we need to restore this session
+
+                    fs.writeFileSync('tokens/auth_info.json', JSON.stringify(authInfo, null, '\t')) // save this info to a file
+
+                    resolve()
+
+                } catch (error) {
+                    reject(error)
+                }
+            }
+        )
+    }
+
+    async loadConnection() {
+        this.conn = new WAConnection()
+        this.conn.loadAuthInfo('tokens/auth_info.json') // will load JSON credentials from file
+
+        try {
+
+            await this.conn.connect()
+        } catch (error) {
+            throw new Error(error.message);
+
+        }
+    }
+
+
 }
